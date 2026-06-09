2872ed8426036cc8e7caffdf195f9adbe142e225
ALVARO-UTN <alvaro.srr@gmail.com>
2026-06-09 09:25:24 -0300

    estructura

---
+++ src/main/java/com/ProgIV/Prode/ProdeApplication.java
@@ -0,0 +1,13 @@
+package com.ProgIV.Prode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdeApplication.class, args);
+	}
+
+}
