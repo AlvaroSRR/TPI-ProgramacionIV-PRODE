625f67744016900095aead13079b1c1568ce1a37
ALVARO-UTN <alvaro.srr@gmail.com>
2026-06-10 09:35:42 -0300

    [feat] modelos completos

--- src/main/java/com/ProgIV/Prode/features/models/Partido.java
+++ src/main/java/com/ProgIV/Prode/features/models/Partido.java
@@ -1,16 +1,23 @@
 package com.ProgIV.Prode.features.models;
 
-import java.time.LocalTime;
+import java.time.OffsetDateTime;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
-// terminar de definir
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Partido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,20 +27,28 @@ public class Partido {
     @ManyToOne
     @JoinColumn(name = "idLocal", nullable = false)
     private Equipo equipoLocal;
+
     @NotNull
     @ManyToOne
     @JoinColumn(name = "idVisitante", nullable = false)
     private Equipo equipoVisitante;
+
     @NotNull
     private Integer golLocal;
+
     @NotNull
     private Integer golVisitante;
 
     @NotNull
     private EstadoPartido estadoPartido;
+
     @NotNull
     private String resultado;
     
+    @ManyToOne
+    @JoinColumn(name = "idFecha", nullable = false)
     private Fecha fecha;
-    private LocalTime horaInicio;
+
+    @NotNull
+    private OffsetDateTime horaInicio; // formato UTC
 }
