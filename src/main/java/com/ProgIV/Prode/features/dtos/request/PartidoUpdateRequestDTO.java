051f7b14ffd5470b58dcfe182d9a4dc2c5434797
Alvaro Rivera <alvaro.srr@gmail.com>
2026-06-22 18:49:15 -0300

    [feat]carga de resultado de partido

---
+++ src/main/java/com/ProgIV/Prode/features/dtos/request/PartidoUpdateRequestDTO.java
@@ -0,0 +1,13 @@
+package com.ProgIV.Prode.features.dtos.request;
+
+
+public record PartidoUpdateRequestDTO(
+
+    Long partidoId,
+    Integer golLocal,
+    Integer golVisitante
+
+) {
+    
+}
+
