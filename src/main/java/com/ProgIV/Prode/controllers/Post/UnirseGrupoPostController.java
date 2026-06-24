0b16b0a2cade820e2953e4d6bb06311b24a59292
DavidUtnProg1 <druiz0073@gmail.com>
2026-06-24 09:55:19 -0300

    GrupoJoin listo

---
+++ src/main/java/com/ProgIV/Prode/controllers/Post/UnirseGrupoPostController.java
@@ -0,0 +1,33 @@
+package com.ProgIV.Prode.controllers.Post;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ProgIV.Prode.features.dtos.request.UnirseGrupoRequestDTO;
+import com.ProgIV.Prode.features.services.impl.grupo.GrupoJoinService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/grupos")
+@RequiredArgsConstructor
+public class UnirseGrupoPostController {
+
+    private final GrupoJoinService grupoJoinService;
+
+    @PostMapping("/unirse/{usuarioId}")
+    public ResponseEntity<String> unirseGrupo(
+            @PathVariable Long usuarioId,
+            @RequestBody UnirseGrupoRequestDTO request) {
+        grupoJoinService.unirse(
+            usuarioId,
+            request.getCodigoInvitacion()
+        );
+        
+            return ResponseEntity.ok("Usuario unido al grupo exitosamente");
+    }
+}
